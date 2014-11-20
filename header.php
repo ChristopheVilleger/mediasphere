942d175df1314d15fcc45bad836ce124df3879e6
rYassine <riffi.yassine@gmail.com>
2014-11-20 21:43:11 +0100

    Mise à jour

--- header.php
+++ header.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <head>
-    <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-    <title><?php wp_title( '|', true, 'right' ); ?></title>
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
-    <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
-    <?php wp_head(); ?>
+	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-    <div id="page-wrap">
-    <div id="header">
-        <h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
-        <div class="description"><?php bloginfo('description'); ?></div>
-    </div>
+	<div id="page-wrap">
+		<div id="header">
+			<h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
+			<div class="description"><?php bloginfo('description'); ?></div>
+		</div>
 
+		<?php echo ms_homepage();?>
+		<!-- Posts  -->
+		<!-- Posts  -->
---
+++ inc/homepage.inc.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Homepage functions.
+ *
+ * @package MediaSphere
+ */
+
+function ms_homepage( ) {
+
+	echo ms_top_menu();
+
+	echo ms_featured();
+
+	echo ms_categories();
+
+}
+
+/**
+*
+* Menu top
+*
+* Show top menu
+*
+**/
+function ms_top_menu( ) {
+	echo'<!-- Head Menu  -->';
+
+	echo'<!-- END Head Menu  -->';
+}
+
+function ms_featured( ) {
+
+	echo'<!-- Featured  -->';
+
+	echo '<!-- Featured  -->';
+}
+function ms_categories( ) {
+
+	echo'<!-- Categories  -->';
+
+	echo'<!-- Categories  -->';
+}
