9b487b2399ace6c4544ab97202fcb1c305ca1670
rYassine <riffi.yassine@gmail.com>
2014-11-23 16:00:21 +0100

    Mises à jour Yssine
    Maj

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
-            <div id="footer">&copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?></div>
-	</div>
-    <?php wp_footer(); ?>	
-</body>
-</html>
+						<div id="footer">&copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?></div>
+					</div>
+					<?php wp_footer(); ?>
+				</body>
+				</html>
